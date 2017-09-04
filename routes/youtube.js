3e976b686ab6aacde3143610eed0d9afcc22aeb7
Giani Noyez <giani.noyez@outlook.com>
2017-09-04 17:06:56 +0200

    basic youtube functionality

--- routes/youtube.js
+++ routes/youtube.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const Youtube = require("youtube-api")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('youtube/index', { title: 'Youtube' });
+    var data = null;
+    
+    Youtube.authenticate({
+        type: "key"
+      , key: ""
+    });
+
+    //,contentDetails,statistics 
+    // with id
+
+    Youtube.search.list({part:'snippet', q: 'ariana grande', maxResults: 50, order: 'date'}, function(err, resp){
+        if(err){
+            console.log(err)
+            return;
+        }
+
+        for(var i = 0; i < resp.items.length; i++){
+            if(resp.items[i].id.kind === 'youtube#video'){
+                console.log('['+ i +'] https://www.youtube.com/watch?v=' + resp.items[i].id.videoId + '\t '  + resp.items[i].snippet.publishedAt + '\t ' + resp.items[i].snippet.title)
+            }
+        }
+        data = resp.items;
+        console.log(data[0].snippet.thumbnails)
+        res.render('youtube/index', { data });
+    });
+
+    
 });
 
 module.exports = router;
