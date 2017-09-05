878d4cd108fdfcef0f2a1dbe243d164890a55e07
Giani Noyez <giani.noyez@outlook.com>
2017-09-05 13:55:02 +0200

    added a youtube route and form

--- routes/youtube.js
+++ routes/youtube.js
@@ -14,21 +14,25 @@ router.get('/', function(req, res, next) {
     //,contentDetails,statistics 
     // with id
 
-    Youtube.playlistItems.list({part:'snippet,contentDetails', playlistId: 'UUXuqSBlHAE6Xw-yeJA0Tunw', maxResults: 10, order: 'videoPublishedAt'}, function(err, resp){
-        if(err){
-            console.log(err)
-            return;
-        }
+    // Youtube.playlistItems.list({part:'snippet,contentDetails', playlistId: 'UUXuqSBlHAE6Xw-yeJA0Tunw', maxResults: 10, order: 'videoPublishedAt'}, function(err, resp){
+    //     if(err){
+    //         console.log(err)
+    //         return;
+    //     }
 
-        for(var i = 0; i < resp.items.length; i++){
-            if(resp.items[i].id.kind === 'youtube#playlistItem'){
-                console.log('['+ i +'] https://www.youtube.com/watch?v=' + resp.items[i].id.videoId + '\t '  + resp.items[i].snippet.publishedAt + '\t ' + resp.items[i].snippet.title)
-            }
-        }
-        data = resp.items;
+    //     for(var i = 0; i < resp.items.length; i++){
+    //         if(resp.items[i].id.kind === 'youtube#playlistItem'){
+    //             console.log('['+ i +'] https://www.youtube.com/watch?v=' + resp.items[i].id.videoId + '\t '  + resp.items[i].snippet.publishedAt + '\t ' + resp.items[i].snippet.title)
+    //         }
+    //     }
+        data = [];
         //console.log(resp.items)
         res.render('youtube/index', { data });
-    });  
+    // });  
 });
+
+router.post('/addChannel', function(req, res, next){
+
+})
 
 module.exports = router;
